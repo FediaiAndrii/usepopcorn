29266e77a049713ed11332b98282f019438ab300
fediaiandrii <lezukokitagava@gmail.com>
2026-06-19 14:48:49 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ export default function App() {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&S=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&S=${query}`,
             { signal: controller.signal },
           );
 
